3b87d8497fcfe64d14418746ea9d6447f1ca76c3
JASKAMRA <kamrajas26@gmail.com>
2025-11-27 00:59:45 +0530

    comitted

--- src/edu/univ/erp/domain/Status.java
+++ src/edu/univ/erp/domain/Status.java
@@ -1,9 +1,6 @@
 package edu.univ.erp.domain;
 
 public enum Status {
-    Pending,
-    Confirmed,
-    ENROLLED,      // <-- MUST MATCH DB VALUE EXACTLY (UPPERCASE)
-    waitlisted,
+    ENROLLED,     
     dropped
 }
